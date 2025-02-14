28fb43700ac2d0dfcd2d3b495a2bc63208097f65
seus31 <us.balut.big.dipper@gmail.com>
2025-02-14 10:11:38 +0900

    登録APIのレスポンス後の処理にdashboardへのリダイレクト追加及び不要な;の削除

--- frontend/src/app/components/pages/authentication/auth-signup/auth-signup.component.ts
+++ frontend/src/app/components/pages/authentication/auth-signup/auth-signup.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { AuthService } from '../../../../auth.service';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { Component } from '@angular/core'
+import {Router, RouterModule} from '@angular/router'
+import { AuthService } from '../../../../auth.service'
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { NgIf } from '@angular/common'
 
 @Component({
   selector: 'app-auth-signup',
@@ -12,30 +12,31 @@ import { NgIf } from '@angular/common';
   styleUrls: ['./auth-signup.component.scss']
 })
 export default class AuthSignupComponent {
-  registerForm: FormGroup;
+  registerForm: FormGroup
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
     this.registerForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
-    });
+    })
   }
 
   onSubmit() {
     if (this.registerForm.valid) {
       this.authService.register(this.registerForm.value).subscribe({
         next: (response) => {
-          this.authService.saveToken(response.token);
-          console.log('Logged in successfully');
+          this.authService.saveToken(response.token)
+          this.router.navigate(['/dashboard']).then(r => {})
+          console.log('Logged in successfully')
         },
         error: (error) => {
-          console.error('Login failed', error);
+          console.error('Login failed', error)
         },
         complete: () => {
-          console.log('Request completed');
+          console.log('Request completed')
         }
-      });
+      })
     }
   }
 }
