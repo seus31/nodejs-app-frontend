b78de0921c0dd33d21b849b29c2010ca8d69c52e
seus31 <us.balut.big.dipper@gmail.com>
2025-02-14 10:10:25 +0900

    タスク一覧のメニューリンク追加

--- frontend/src/app/theme/layout/admin/navigation/navigation.ts
+++ frontend/src/app/theme/layout/admin/navigation/navigation.ts
@@ -1,24 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 
 export interface NavigationItem {
-  id: string;
-  title: string;
-  type: 'item' | 'collapse' | 'group';
-  translate?: string;
-  icon?: string;
-  hidden?: boolean;
-  url?: string;
-  classes?: string;
-  exactMatch?: boolean;
-  external?: boolean;
-  target?: boolean;
-  breadcrumbs?: boolean;
-  function?: any;
-  children?: Navigation[];
+  id: string
+  title: string
+  type: 'item' | 'collapse' | 'group'
+  translate?: string
+  icon?: string
+  hidden?: boolean
+  url?: string
+  classes?: string
+  exactMatch?: boolean
+  external?: boolean
+  target?: boolean
+  breadcrumbs?: boolean
+  function?: any
+  children?: Navigation[]
 }
 
 export interface Navigation extends NavigationItem {
-  children?: NavigationItem[];
+  children?: NavigationItem[]
 }
 
 const NavigationItems = [
@@ -35,14 +35,22 @@ const NavigationItems = [
         url: '/dashboard/home',
         icon: 'feather icon-home',
         classes: 'nav-item'
+      },
+      {
+        id: 'tasks',
+        title: 'Tasks',
+        type: 'item',
+        url: '/dashboard/tasks/list',
+        icon: 'feather icon-list',
+        classes: 'nav-item'
       }
     ]
   },
-];
+]
 
 @Injectable()
 export class NavigationItem {
   get() {
-    return NavigationItems;
+    return NavigationItems
   }
 }
