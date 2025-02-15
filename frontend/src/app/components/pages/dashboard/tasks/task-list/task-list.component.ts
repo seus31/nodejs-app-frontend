4545e7108797f10c87d55963a2113fe4d15d406f
seus31 <us.balut.big.dipper@gmail.com>
2025-02-15 10:41:54 +0900

    task一覧コンポーネントにAPIからタスクの取得をする処理を追加とtable表示するように修正

--- frontend/src/app/components/pages/dashboard/tasks/task-list/task-list.component.ts
+++ frontend/src/app/components/pages/dashboard/tasks/task-list/task-list.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { ReactiveFormsModule } from '@angular/forms'
+import { TaskService } from '../../../../../services/task.service'
+import { TableComponent } from '../../../../../theme/shared/components/table/table.component'
 
 @Component({
   selector: 'dashboard',
   standalone: true,
-  imports: [RouterModule, ReactiveFormsModule],
+  imports: [RouterModule, ReactiveFormsModule, TableComponent],
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss']
 })
-export default class TaskListComponent {}
+export default class TaskListComponent implements OnInit {
+  tasks: any[] = []
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {
+    this.loadTasks(1, 10)
+  }
+
+  loadTasks(page: number, perPage: number) {
+    this.taskService.list(page, perPage).subscribe((response) => {
+      this.tasks = response.tasks
+    })
+  }
+
+  columns = [
+    { key: 'id', label: 'ID' },
+    { key: 'task_name', label: 'Title' },
+    { key: 'status', label: 'Status' },
+  ]
+}
